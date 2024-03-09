ea8cee533510ff6e017e544de60c0e94daeeb7ef
acze <artur.czekaj99@gmail.com>
2024-03-09 13:16:13 +0100

    ubuntu version

--- check.py
+++ check.py
@@ -1,26 +1,48 @@
-import pyttsx3
-import time
+import cups
+import tkinter as tk
 from tkinter import messagebox
+import time
 
-def sprawdz_drukarke():
-    podlaczona = True
-    toner = True
-    tusz = True
 
-    return podlaczona, toner, tusz
+def sprawdz_drukarke(prefix):
+    conn = cups.Connection()
+    printers = conn.getPrinters()
 
-def informuj_o_stanie_drukarki(podlaczona, toner, tusz):
-    engine = pyttsx3.init()
+    for printer in printers:
+        if printer.startswith(prefix):
+            printer_info = printers[printer]
+            status = printer_info['printer-state']
+            toner = printer_info.get('printer-toner-low', False)
+            ink = printer_info.get('printer-ink-low', False)
+
+            return True, status == 3, toner, ink
+
+    return False, False, False, False
+
+
+def informuj_o_stanie_drukarki():
+    prefix = "HP"
+    podlaczona, offline, toner, tusz = sprawdz_drukarke(prefix)
+
     if not podlaczona:
         messagebox.showerror("Błąd", "Uwaga! Drukarka nie jest podłączona.")
+    elif offline:
+        messagebox.showerror("Błąd", "Uwaga! Drukarka jest offline.")
     else:
         if toner:
             messagebox.showerror("Błąd", "Uwaga! Brakuje tonera w drukarce.")
         if tusz:
             messagebox.showerror("Błąd", "Uwaga! Brakuje tuszu w drukarce.")
 
-if __name__ == "__main__":
+
+def sprawdz_co_minute():
     while True:
-        podlaczona, toner, tusz = sprawdz_drukarke()
-        informuj_o_stanie_drukarki(podlaczona, toner, tusz)
-        time.sleep(60)
+        informuj_o_stanie_drukarki()
+        time.sleep(15)
+
+
+
+root = tk.Tk()
+root.withdraw()
+sprawdz_co_minute()
+root.mainloop()
