81f1cc8de90eb7c2f0a168c57609c37a041dcf84
acze <artur.czekaj99@gmail.com>
2024-03-09 09:51:17 +0100

    msg_box change

--- check.py
+++ check.py
@@ -1,5 +1,6 @@
 import pyttsx3
 import time
+from tkinter import messagebox
 
 def sprawdz_drukarke():
     podlaczona = True
@@ -11,19 +12,15 @@ def sprawdz_drukarke():
 def informuj_o_stanie_drukarki(podlaczona, toner, tusz):
     engine = pyttsx3.init()
     if not podlaczona:
-        engine.say("Uwaga! Drukarka nie jest podłączona.")
+        messagebox.showerror("Błąd", "Uwaga! Drukarka nie jest podłączona.")
     else:
-        if not toner:
-            engine.say("Uwaga! Brakuje tonera w drukarce.")
-        if not tusz:
-            engine.say("Uwaga! Brakuje tuszu w drukarce.")
-        if toner and tusz:
-            engine.say("Drukarka jest gotowa do pracy.")
-    engine.runAndWait()
+        if toner:
+            messagebox.showerror("Błąd", "Uwaga! Brakuje tonera w drukarce.")
+        if tusz:
+            messagebox.showerror("Błąd", "Uwaga! Brakuje tuszu w drukarce.")
 
 if __name__ == "__main__":
     while True:
         podlaczona, toner, tusz = sprawdz_drukarke()
         informuj_o_stanie_drukarki(podlaczona, toner, tusz)
-        # Odczekaj 60 sekund (1 minuta) przed ponownym sprawdzeniem
         time.sleep(60)
